24dd8fc170b3f5f369a392e464fab102cb283cc2
zalewa10 <61093642+zalewa10@users.noreply.github.com>
2023-01-12 21:52:18 +0100

    Add files via upload

---
+++ script-login.js
@@ -0,0 +1,36 @@
+user = document.getElementById("Użytkownik");
+password = document.getElementById("Hasło");
+loginBtn = document.getElementById("login");
+message = document.getElementById("response");
+card = document.querySelector(".card");
+let userBase = [
+  { username: "KrzysztofZal", userpass: "Masło" },
+  { username: "TymoteuszStr", userpass: "MojeHasło" },
+];
+
+let validUser = false;
+
+loginBtn.addEventListener("click", () => {
+  enteredPasword = password.value;
+  enteredEmail = user.value;
+  userBase.forEach((currUser) => {
+    console.log(enteredEmail, enteredPasword);
+    if (
+      currUser.username == enteredEmail &&
+      currUser.userpass == enteredPasword
+    ) {
+      console.log("Poprawne");
+      validUser = true;
+    } else {
+      console.log("Niepoprawne");
+    }
+  });
+  console.log(validUser);
+  if (validUser == true) {
+    console.log("tu działa");
+    window.location.href = "index2.html";
+  } else {
+    card.style.height = "350px";
+    message.innerHTML = "Niepoprawne hasło lub email";
+  }
+});
