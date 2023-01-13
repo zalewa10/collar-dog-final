018dbca3794dd79084e035253f1994da195b857a
zalewa10 <61093642+zalewa10@users.noreply.github.com>
2023-01-13 17:35:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// write your JavaScript here
+const sidebarToggle = document.querySelector(".navbar-toggle");
+const sidebar = document.querySelector(".navbar-in");
+const closebtn = document.querySelector(".close-btn");
+
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("show-navbar");
+  console.log("ee");
+});
+
+closebtn.addEventListener("click", () => {
+  sidebar.classList.remove("show-navbar");
+});
